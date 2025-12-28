da06fa4f977b44e0b8ab54597d2d850dfb13b184
kmz16 <mykhantmhuuzay@gmail.com>
2025-12-28 11:01:51 +0630

    Create short calculator.py

---
+++ short calculator.py
@@ -0,0 +1,12 @@
+m=float(input("one code"))
+k=float(input("two code"))
+
+a=int(m+k)
+b=int(m+k)
+c=int(m*k)
+d=int(m/k)
+
+print(f"+ ans {a}") 
+print(f"- ans {b}")
+print(f"/ ans {c}")
+print(f"* ans {d}")
