c6a7c3c93f68e9593f456eb7b473b5bbe1b70817
kmz16 <mykhantmhuuzay@gmail.com>
2025-12-28 11:05:05 +0630

    Update short calculator.py

--- short calculator.py
+++ short calculator.py
@@ -1,10 +1,10 @@
 m=float(input("one code"))
 k=float(input("two code"))
 
-a=int(m+k)
-b=int(m+k)
-c=int(m*k)
-d=int(m/k)
+a=str(m+k)
+b=str(m+k)
+c=str(m*k)
+d=str(m/k)
 
 print(f"+ ans {a}") 
 print(f"- ans {b}")
